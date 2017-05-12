da7d62e0b6739d2e62c15ab564bfedf6da24457d
Jason <1031500191@qq.com>
2017-05-12 12:33:48 +0800

    自定义前置通知类

---
+++ src/edu/jyu/advice/MyBeforeAdvice.java
@@ -0,0 +1,18 @@
+package edu.jyu.advice;
+
+import java.lang.reflect.Method;
+
+import edu.jyu.aop.MethodBeforeAdvice;
+
+/**
+ * 自定义前置通知类
+* @author Jason
+ */
+public class MyBeforeAdvice implements MethodBeforeAdvice {
+
+	@Override
+	public void before(Method method, Object[] args, Object target) {
+		System.out.println("前置通知");
+	}
+
+}
