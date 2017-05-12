2dcb6c37b32877ac7f8083f01767746417efa181
Jason <1031500191@qq.com>
2017-05-12 12:32:52 +0800

    目标对象相关类和接口

--- src/edu/jyu/dao/UserDao.java
+++ src/edu/jyu/dao/UserDao.java
@@ -2,4 +2,6 @@ package edu.jyu.dao;
 
 public interface UserDao {
 	public void add(String user);
+
+	public String getUser(String id);
 }
--- src/edu/jyu/dao/UserDaoImpl.java
+++ src/edu/jyu/dao/UserDaoImpl.java
@@ -1,9 +1,15 @@
 package edu.jyu.dao;
 
-public class UserDaoImpl implements UserDao{
+public class UserDaoImpl implements UserDao {
 
 	@Override
 	public void add(String user) {
-		System.out.println("add "+user);
+		System.out.println("add " + user);
+	}
+
+	@Override
+	public String getUser(String id) {
+		System.out.println("getUser " + id);
+		return id + ":Jason";
 	}
 }
